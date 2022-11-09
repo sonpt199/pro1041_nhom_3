609f215ad1260d318495f9931ec72ea9c5f19e95
sonptph19600 <sonptph19600@fpt.edu.vn>
2022-11-09 10:30:21 +0700

    Before 1

--- src/main/java/fpoly/pro1041/nhom_3/application/Application.java
+++ src/main/java/fpoly/pro1041/nhom_3/application/Application.java
@@ -1,9 +1,16 @@
 package fpoly.pro1041.nhom_3.application;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author sonpt_ph19600
  */
 public class Application {
-    
+    public static void main(String[] args) {
+        String a = null;
+        System.out.println(a.equals("b"));
+    }
 }
